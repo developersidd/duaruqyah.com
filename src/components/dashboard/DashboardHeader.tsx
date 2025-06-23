c4e50f0b594d090e8d3c7247d3f892299428fd44
Emon8794 <emonabujafor@gmail.com>
2025-06-24 01:49:16 +0600

    fix: remove unnecessary comment in DashboardHeader component

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -8,7 +8,6 @@ const DashboardHeader = ({ children }: { children: ReactNode }) => {
     <header className="flex justify-between mx-auto py-5 items-center px-3 sm:px-5 bg-white sticky top-0 right-0 w-full h-[60px]">
       {children}
       <div className="flex items-center gap-[10px] sm:gap-3 lg:gap-5">
-        {/* Theme */}
 
         <Button
           btnText="সাপোর্ট করুন"
